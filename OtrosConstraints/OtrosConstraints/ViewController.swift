3f6d042a7525dedf6c0245b1e365a216169a874c
rech99 <53828060+rech99@users.noreply.github.com>
2021-09-09 17:29:15 -0700

    Add files via upload

---
+++ OtrosConstraints/OtrosConstraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OtrosConstraints
+//
+//  Created by Alumno on 9/9/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
